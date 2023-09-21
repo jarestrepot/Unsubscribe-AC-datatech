77fa2fb69762d9e51f175fc4ddfa1afec0343408
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2023-09-21 22:30:36 +0200

    first commit

---
+++ unsubcribeJS/unsubcribe.js
@@ -0,0 +1,53 @@
+const conatinerMain = document.querySelector('.conatinerUnsubscribe')
+const modalRequest = document.querySelector('.modalHidden')
+const buttonModal = document.querySelector('.closeModal')
+const spinnerModal = document.querySelector('.sppiner')
+
+const unsubscribeCallback = (evento) => {
+  const urlSearchParams = new URLSearchParams(window.location.search)
+  const params = Object.fromEntries(urlSearchParams.entries())
+  const { token } = params
+  if (!token) {
+    activeModal('No se puede realizar la petición. Usuario no identificado!')
+  }
+  fechingUnsubcribe(token)
+  evento.target.setAttribute('disabled', '')
+}
+document.getElementById('bajaACdatatech').addEventListener('click', unsubscribeCallback)
+const API_AC = 'http://localhost:3003/client/unsubscribe'
+
+const fechingUnsubcribe = (token) => {
+  conatinerMain.classList.add('opacity-50')
+  spinnerModal.classList.remove('hidden')
+  fetch(API_AC, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then((response) => {
+      if (response.ok) {
+        activeModal('Response ok usuario eliminado!!')
+      }
+    })
+    .catch((error) => {
+      console.log('Error ❌ ', error)
+      activeModal('No encontrado!!')
+    })
+}
+
+const activeModal = (text) => {
+  conatinerMain.classList.add('opacity-50')
+  modalRequest.classList.remove('hidden')
+  const sectionModal = document.querySelectorAll('section')
+  sectionModal[0].children[0].innerText = text
+  disabledSpinner()
+}
+
+buttonModal.addEventListener('click', () => {
+  conatinerMain.classList.remove('opacity-50')
+  modalRequest.classList.add('hidden')
+  disabledSpinner()
+})
+
+const disabledSpinner = () => spinnerModal.classList.add('hidden')
